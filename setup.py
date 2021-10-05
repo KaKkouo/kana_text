f80cddfb8fc1bf474e3639b5dfc5179974d14b70
koKekkoh <raoluba@outlook.jp>
2021-10-06 05:23:37 +0900

    0.22.0

--- setup.py
+++ setup.py
@@ -15,5 +15,5 @@ setup(
         'Knowledge': 'https://qiita.com/tags/sphinxcotrib.kana_text',
         'Code': 'https://github.com/KaKkouo/sphinxcontrib.kana_text',
         'Issue tracker': 'https://github.com/KaKkouo/sphinxcontrib.kana_text/issues',
-    },
+    }
 )
