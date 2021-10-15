eed5a7baecd3f84ef64ffa22400fd32cae20a344
koKekkoh <raoluba@outlook.jp>
2021-10-15 09:59:33 +0900

    split to unittest_04.py, unittest_05.py, unittest_06.py and unittest_07.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,14 +1,22 @@
 import unittest
 
-from unittest_01 import testKanaText
-from unittest_02 import testKanaTextUnit
-from unittest_03 import testIndexUnit
-from unittest_04 import testIndexRack
+from unittest_01 import testKanaText as KanaText
+from unittest_02 import testKanaTextUnit as KanaTextUnit
+from unittest_03 import testIndexUnit as IndexUnit
+from unittest_04 import testIndexRack as IndexRack1
+from unittest_05 import testIndexRack as IndexRack2
+from unittest_06 import testIndexRack as IndexRack3
+from unittest_07 import testIndexRack as IndexRack4
+from unittest_08 import testIndexRack as IndexRack5
 
 def suites():
     suites = unittest.TestSuite()
-    suites.addTests(unittest.makeSuite(testKanaText))
-    suites.addTests(unittest.makeSuite(testKanaTextUnit))
-    suites.addTests(unittest.makeSuite(testIndexUnit))
-    suites.addTests(unittest.makeSuite(testIndexRack))
+    suites.addTests(unittest.makeSuite(KanaText))
+    suites.addTests(unittest.makeSuite(KanaTextUnit))
+    suites.addTests(unittest.makeSuite(IndexUnit))
+    suites.addTests(unittest.makeSuite(IndexRack1))
+    suites.addTests(unittest.makeSuite(IndexRack2))
+    suites.addTests(unittest.makeSuite(IndexRack3))
+    suites.addTests(unittest.makeSuite(IndexRack4))
+    suites.addTests(unittest.makeSuite(IndexRack5))
     return suites
