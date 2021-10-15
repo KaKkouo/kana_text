3a0c6543910570b3d18be3fda9b9b17ddc89fb4b
koKekkoh <raoluba@outlook.jp>
2021-10-16 05:24:56 +0900

    0.24.0.4

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     download_url = 'https://pypi.org/project/sphinxcontrib.kana-text/',
     project_urls = {
         'Knowledge': 'https://qiita.com/tags/sphinxcotrib.kana_text',
-        'Code': 'https://github.com/KaKkouo/sphinxcontrib.kana_text',
-        'Issue tracker': 'https://github.com/KaKkouo/sphinxcontrib.kana_text/issues',
+        'Code': 'https://github.com/KaKkouo/kana_text',
+        'Issue tracker': 'https://github.com/KaKkouo/kana_text/issues',
     }
 )
