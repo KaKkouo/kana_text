1bb851fa48684aa6a4e98d6ea327b6524c7e5cf9
koKekkoh <raoluba@outlook.jp>
2021-10-22 04:05:15 +0900

    0.25.0.0.20211022 rename.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,17 +1,18 @@
 import unittest
 
-from . import testcase01, testcase02, testcase03, testcase04, testcase05
-from . import testcase06, testcase07, testcase08, testcase09
+from . import testcase01, testcase21, testcase31
+from . import testcase41, testcase43, testcase61, testcase71
+from . import testcase81, testcase91
 
 def suites():
     suites = unittest.TestSuite()
     suites.addTests(unittest.makeSuite(testcase01.testKanaText))
-    suites.addTests(unittest.makeSuite(testcase02.testKanaTextUnit))
-    suites.addTests(unittest.makeSuite(testcase03.testIndexUnit))
-    suites.addTests(unittest.makeSuite(testcase04.testIndexRack))
-    suites.addTests(unittest.makeSuite(testcase05.testIndexRack))
-    suites.addTests(unittest.makeSuite(testcase06.testIndexRack))
-    suites.addTests(unittest.makeSuite(testcase07.testIndexRack))
-    suites.addTests(unittest.makeSuite(testcase08.testIndexRack))
-    suites.addTests(unittest.makeSuite(testcase09.testIndexEntries))
+    suites.addTests(unittest.makeSuite(testcase21.testKanaTextUnit))
+    suites.addTests(unittest.makeSuite(testcase31.testIndexUnit))
+    suites.addTests(unittest.makeSuite(testcase41.testIndexRack))
+    suites.addTests(unittest.makeSuite(testcase43.testIndexRack))
+    suites.addTests(unittest.makeSuite(testcase61.testIndexRack))
+    suites.addTests(unittest.makeSuite(testcase71.testIndexRack))
+    suites.addTests(unittest.makeSuite(testcase81.testIndexRack))
+    suites.addTests(unittest.makeSuite(testcase91.testIndexEntries))
     return suites
