ce16b7e25e24763ff221bdc4f591c412a44eeec3
koKekkoh <raoluba@outlook.jp>
2021-10-25 16:23:35 +0900

    0.25.1.1

--- src/__init__.py
+++ src/__init__.py
@@ -186,7 +186,7 @@ latexの関連情報
 __copyright__ = 'Copyright (C) 2021 @koKkekoh'
 __license__ = 'BSD 2-Clause License'
 __author__  = '@koKekkoh'
-__version__ = '0.25.1.1b1' # 2021-10-25
+__version__ = '0.25.1.1' # 2021-10-25
 __url__     = 'https://qiita.com/tags/sphinxcotrib.kana_text'
 
 import re, pathlib
