285d575a933463d7cbcb5e38b3dbb04353004aa8
koKekkoh <raoluba@outlook.jp>
2021-10-25 10:24:06 +0900

    separator, option_marker. and rename.

---
+++ tests/testcase06.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+import sys
+import unittest
+sys.path.append('sphinxcontrib')
+from src import KanaText
+
+from . import dataset0
+
+#askana()の基本チェック
+testcase01i = dataset0.dataset
+
+testcase01o = [   #想定する結果
+    "よみ１", "よみ２", "よみ３", "よみ４", "よみ５", "", "", "", "", "",
+    "よみ１", "よみ２", "よみ３", "よみ４", "よみ５", "", "", "", "", "",
+    "よみ１", "よみ２", "よみ３", "よみ４", "よみ５", "", "", "", "", "",
+    "", "", ""
+    ]
+
+class testKanaText(unittest.TestCase):
+    #askanaの基本チェック
+    def test01_askana(self):
+        for t, e in zip(testcase01i, testcase01o):
+            term = KanaText(t)
+            rslt = term.askana()
+            self.assertEqual(e, rslt)
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    unittest.main()
