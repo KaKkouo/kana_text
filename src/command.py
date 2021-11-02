065e115f6625a87879064b02319e1b089c77a032
koKekkoh <raoluba@outlook.jp>
2021-11-02 21:34:48 +0900

    add a return.

--- src/command.py
+++ src/command.py
@@ -8,11 +8,12 @@ def genindex():
     file_name = 'genindex.html.sample'
     read_file = dirname(__file__) + '/templates/'+file_name
 
+    print("")
     fd = open(read_file, 'r')
     jinja = fd.read()
     fd.close()
 
-    print('creating:', file_name, end=' . . . ')
+    print('creating:', file_name, end=' ... ')
     fd = open(file_name, 'w')
     fd.write(jinja)
     fd.close()
