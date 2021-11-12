a8a349cc0dd402d359d4bb9f429d5fb522b292ab
koKekkoh <raoluba@outlook.jp>
2021-11-13 02:17:16 +0900

    upgrading for tox.

---
+++ upgrade.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash -x
+
+.tox/py36/bin/python -m pip install --upgrade sphindexer
+.tox/py37/bin/python -m pip install --upgrade sphindexer
+.tox/py38/bin/python -m pip install --upgrade sphindexer
+
