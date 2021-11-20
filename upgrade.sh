08c7513c23523dfd797f7b261a88b30b5a8f47c9
koKekkoh <raoluba@outlook.jp>
2021-11-20 15:11:24 +0900

    add Sphinx pypi.

--- upgrade.sh
+++ upgrade.sh
@@ -4,6 +4,6 @@ readonly LIST="py36 py37 py38"
 
 for v in $LIST
 do
-	.tox/${v}/bin/python -m pip install --upgrade pytest pytest-cov sphindexer
+	.tox/${v}/bin/python -m pip install --upgrade pip Sphinx pytest pytest-cov sphindexer
 done
 
