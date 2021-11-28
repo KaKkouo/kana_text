0dad300decabd10eee6de9bba5a0853b4c6f473b
koKekkoh <raoluba@outlook.jp>
2021-11-29 07:02:42 +0900

    0.30.2.1a0: purge 'import src'.

--- setup.py
+++ setup.py
@@ -1,13 +1,7 @@
 import sys
 from setuptools import setup
 
-import src as kt
-
 setup(
-    version = kt.__version__,
-    license = kt.__license__,
-    author = kt.__author__,
-    url = kt.__url__,
     download_url = 'https://pypi.org/project/sphinxcontrib.kana-text/',
     project_urls = {
         'Knowledge': 'https://qiita.com/tags/sphinxcotrib.kana_text',
